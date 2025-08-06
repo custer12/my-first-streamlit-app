503b21a995de2109f1e421bb1d125e9033244497
custer12 <189868002+custer12@users.noreply.github.com>
2025-08-06 13:18:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -13,3 +13,11 @@ with col3:
 
 with col4:
     st.header("테스트4")
+
+import streamlit as st
+
+left, middle, right = st.columns(3, vertical_alignment="bottom")
+
+left.text_input("Write something")
+middle.button("Click me", use_container_width=True)
+right.checkbox("Check me")
