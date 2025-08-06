a48f6b1af0435527153cb7196f1715d5a70a6637
custer12 <189868002+custer12@users.noreply.github.com>
2025-08-07 02:30:41 +0900

    Update app.py

--- app.py
+++ app.py
@@ -200,8 +200,8 @@ with tab1:
                                     }
                                     </style>
                                     """, unsafe_allow_html=True)
+                                    st.markdown(f"[🍽️ 레시피 보기]({recipe['link']})")
                                     st.form_submit_button(f"[🍽️ 레시피 보기]")
-                            st.link_button("[🍽️ 레시피 보기]",recipe['link'])
                         st.markdown(f"## {dish_name} 관련 레시피")
                         st.markdown(f"[[ 더 많이 알아보기 ]](https://www.10000recipe.com/recipe/list.html?q={dish_name.replace(" ", "+")})")
                     else:
