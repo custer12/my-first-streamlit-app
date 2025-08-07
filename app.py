5614df8b2756c605ee47aa51dbe66a7a57734ee6
custer12 <189868002+custer12@users.noreply.github.com>
2025-08-07 21:37:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -87,8 +87,8 @@ with tab1:
     space1 = st.empty()
     # 결과 영역
     if submit:
+        st.markdown("---")
         with st.spinner("요리를 생성 중입니다..."):
-            st.markdown("---")
 
             # 스타일별로 AI에게 줄 추가 설명 문구 정의
             style_description = {
